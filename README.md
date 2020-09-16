3fffbe2ed2524dca4bed4f30c2a01005ea737384
George Adams <georgeadams1995@gmail.com>
2020-09-16 14:18:04 +0100

    intial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# TRSS Parser
+trss-parser is a simple tool for generating a markdown issue used as part of triaging.
+
+[Test Results Summary Service (TRSS)](https://trss.adoptopenjdk.net) is a tool created by [AdoptOpenJDK](https://adoptopenjdk.net) to help with test triaging.
+
+## Installation
+```bash
+npm install -g trss-parser
+```
+
+## Usage
+```bash
+trss-parser <parentId> # Where parentID comes from https://trss.adoptopenjdk.net/
+```
+
+### Example
+```bash
+$ trss-parser 5f60dc9a11d26031c17a9f11
+
+TRSS [link](https://trss.adoptopenjdk.net/buildDetail?parentId=5f60dc9a11d26031c17a9f11&testSummaryResult=failed&buildNameRegex=%5ETest) 
+Build URL https://ci.adoptopenjdk.net/job/build-scripts/job/openjdk15-pipeline/282/ 
+Started by user George Adams 
+
+[**Test_openjdk15_hs_sanity.openjdk_aarch64_linux**](https://ci.adoptopenjdk.net/job/Test_openjdk15_hs_sanity.openjdk_aarch64_linux/48/) 
+jdk_lang_0 => [deep history 6/27 passed](https://trss.adoptopenjdk.net/deepHistory?testId=5f60f83711d26031c17ab408) 
+jdk_util_0 => [deep history 6/27 passed](https://trss.adoptopenjdk.net/deepHistory?testId=5f60f83711d26031c17ab406) 
+
+[**Test_openjdk15_hs_special.functional_x86-64_linux**](https://ci.adoptopenjdk.net/job/Test_openjdk15_hs_special.functional_x86-64_linux/46/) 
+MBCS_Tests_unicode_linux_0 => [deep history 0/27 passed](https://trss.adoptopenjdk.net/deepHistory?testId=5f60f82e11d26031c17ab245) 
+MBCS_Tests_codepoint_linux_0 => [deep history 0/27 passed](https://trss.adoptopenjdk.net/deepHistory?testId=5f60f82e11d26031c17ab177) 
+```
+
+The resulting text can the be pasted into a GitHub [Promote Release template](https://github.com/AdoptOpenJDK/TSC/issues/new/choose).
+
+## How to find the parentId
+Go to https://trss.adoptopenjdk.net/ and select a pipeline run that you are triaging.
+This will take you to a new page such as:
+```
+https://trss.adoptopenjdk.net/buildDetail?parentId=5f60f57611d26031c17aac90
+```
+
+The parentId is the last part of the URL.
---
+++ bin/trss-parser.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict';
+
+const request = require('request');
+const syncRequest = require('sync-request');
+
+let PARENT_ID
+let BUILD_URL
+let STARTED_BY
+let MARKDOWN_TEMPLATE
+
+if (! process.argv[2]) {
+    console.error("ERROR: Please specify the parent ID")
+    process.exit(1)
+} else {
+    PARENT_ID = process.argv[2];
+}
+
+request(`https://trss.adoptopenjdk.net/api/getParents?id=${PARENT_ID}`, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    MARKDOWN_TEMPLATE = `TRSS [link](https://trss.adoptopenjdk.net/buildDetail?parentId=${PARENT_ID}&testSummaryResult=failed&buildNameRegex=%5ETest) \n`
+    BUILD_URL = body[0].buildUrl
+    STARTED_BY = body[0].startBy
+    MARKDOWN_TEMPLATE += `Build URL ${BUILD_URL} \nStarted by ${STARTED_BY} \n`
+
+    const buildNameRegex = "^Test_openjdk.*";
+
+    request(`https://trss.adoptopenjdk.net/api/getAllChildBuilds?parentId=${PARENT_ID}&buildNameRegex=${buildNameRegex}`, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        for (let testGroup of body) {
+            if (testGroup.buildResult !== "SUCCESS") {
+                let TEST_GROUP_NAME = testGroup.buildName
+                let TEST_GROUP_URL = testGroup.buildUrl
+                MARKDOWN_TEMPLATE += `\n[**${TEST_GROUP_NAME}**](${TEST_GROUP_URL}) \n`
+                for (let test of testGroup.tests) {
+                    if (test.testResult === "FAILED") {
+                        let TEST_NAME = test.testName
+                        let TEST_ID = test._id
+                        let historyLink = syncRequest('GET', `https://trss.adoptopenjdk.net/api/getHistoryPerTest?testId=${TEST_ID}&limit=100`)
+                        var history = JSON.parse(historyLink.getBody('utf8'));
+                        let TOTAL_RUNS = 0
+                        let TOTAL_PASSES = 0
+                        for (let testRun of history) {
+                            TOTAL_RUNS += 1
+                            if (testRun.tests.testResult === "PASSED") {
+                                TOTAL_PASSES += 1
+                            }
+                        }
+                        MARKDOWN_TEMPLATE += `${TEST_NAME} => [deep history ${TOTAL_PASSES}/${TOTAL_RUNS} passed](https://trss.adoptopenjdk.net/deepHistory?testId=${TEST_ID}) \n`
+                    }
+                }
+            }
+        }   
+        console.log(MARKDOWN_TEMPLATE)
+    });
+});
+
